5d001c37e89624a519a06fc48ef4142d529d0844
aPaXuC <arkiniet@gmail.com>
2017-07-17 18:30:03 +0300

    16/07/2017

---
+++ App_Code/DBConnection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DBConnection
+/// </summary>
+public class DBConnection
+{
+    private static SqlConnection KGConnection;
+    private static String connString = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+        "AttachDbFilename=\"C:\\Program Files\\Microsoft SQL Server\\MSSQL13.SQLEXPRESS" +
+        "\\MSSQL\\DATA\\KindergardenSolution.mdf\";Integrated Security=True;Connect Timeout=30";
+
+    private static DBConnection instance = null;
+
+    public static DBConnection getInstance()
+    {
+        if (instance == null)
+            instance = new DBConnection();
+        //check();
+        return instance;
+    }
+
+    private DBConnection()
+    {
+        KGConnection = new SqlConnection(connString);
+    }
+
+    public static void check()
+    {
+        SqlCommand comm = new SqlCommand();
+        comm.CommandText = "query11";
+        System.Diagnostics.Debug.WriteLine("Trying to connect DEBUG");
+        comm.CommandType = System.Data.CommandType.StoredProcedure;
+        comm.Connection = KGConnection;
+        comm.Parameters.AddWithValue("@kgID", 13);
+        //comm.Parameters.AddWithValue("@classID", 1);
+        try
+        {
+            KGConnection.Open();
+            System.Data.DataTable toReturn = new System.Data.DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            System.Diagnostics.Debug.WriteLine("Returned #" + da.Fill(toReturn));
+        } catch (Exception)
+        {
+            System.Diagnostics.Debug.WriteLine("Exeption occured DEBUG");
+        } finally
+        {
+            KGConnection.Close();
+        }
+    }
+}
---
+++ App_Code/KGManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ConnManager
+/// </summary>
+public class KGManager
+{
+    private static DBConnection DBConn;
+
+    private KGManager()
+    {
+        DBConn = DBConnection.getInstance();
+    }
+}
