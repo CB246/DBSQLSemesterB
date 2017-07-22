0547e4339c67fc7ff3a48fa6270d92f58b614707
aPaXuC <arkiniet@gmail.com>
2017-07-22 17:31:56 +0300

    22/07/2017

--- aspx/SignUp.aspx.cs
+++ aspx/SignUp.aspx.cs
@@ -10,51 +10,15 @@ public partial class aspx_SignUp : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        KGManager.log("Page_Load happened");
     }
 
     protected void btnSignUp_Click(object sender, EventArgs e)
     {
         KGManager.log("Sugn Up button was clicked");
-           if (tbLogin.Text.Equals(""))
-        {
-            KGManager.log("ID is empty!");
-        }
-        if (!tbLogin.Text.All(char.IsDigit))
-        {
-            KGManager.log("ID isn'n number");
-        }
-           if (tbLogin.Text.Length != 9)
-        {
-            KGManager.log("ID isn't 9 digits");
-        }
-           if (tbFirstName.Text.Equals(""))
-        {
-            KGManager.log("First Name is empty");
-        }
-           if (tbLastName.Text.Equals(""))
-        {
-            KGManager.log("Last Name is empty");
-        }
-           if (rblGender.SelectedIndex == -1)
-        {
-            KGManager.log("Gender wasn't chose");
-        }
-           if (tbDOB.Text.Equals(""))
-        {
-            KGManager.log("DOB is empty");
-        }
-           if (tbPassword.Text.Equals(""))
-        {
-            KGManager.log("Password is empty");
-        }
-           if (tbPasswordConfirm.Text.Equals(""))
-        {
-            KGManager.log("Password confirmation is empty");
-        }
-           if (!tbPassword.Text.Equals(tbPasswordConfirm.Text))
-        {
-            KGManager.log("Password confirmation failed");
-        }
+        Dictionary<String, Object> data = new Dictionary<string, object>();
+        data.Add("parentID", tbLogin.Text);
+        System.Data.DataTable dt = DBConnection.runProcWithResults("checkIfIDExists", data);
+        KGManager.log("signUp after check -> " + dt.Rows.Count);
     }
 }
